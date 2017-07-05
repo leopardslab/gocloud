2b5ce31b8c4cceff7b1de8ee4a458cb494a116c3
PratikDhanave <i.pratikdhanave@gmail.com>
2017-07-05 05:59:40 +0530

    create loadbalancer

--- loadbalancer/awsloadbalancer/loadbalancer.go
+++ loadbalancer/awsloadbalancer/loadbalancer.go
@@ -7,18 +7,81 @@ import (
 	awsauth "github.com/scorelab/gocloud-v2/awsauth"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"time"
 )
 
 type Awsloadbalancer struct {
 }
 
+type CreateLoadBalancer struct {
+	Name           string
+	IpAddressType  string
+	Scheme         string
+	Tags           []Tag
+	SecurityGroups []string
+	Subnets        []string
+}
+
+type Tag struct {
+	Key   string `xml:"key"`
+	Value string `xml:"value"`
+}
+
 func (awsloadbalancer *Awsloadbalancer) Creatloadbalancer(request interface{}) (resp interface{}, err error) {
 
+	var options CreateLoadBalancer
+
+	param := make(map[string]interface{})
+
+	param = request.(map[string]interface{})
+
+	for key, value := range param {
+		switch key {
+
+		case "Name":
+			NameV, _ := value.(string)
+			options.Name = NameV
+
+		case "Subnets":
+			SubnetsV, _ := value.([]string)
+			options.Subnets = SubnetsV
+		}
+	}
+
+	fmt.Println("options :", options)
+
 	params := makeParamsWithVersion("CreateLoadBalancer")
+
+	prepareSubnets(params, options.Subnets)
+
+	if options.Name != "" {
+		params["Name"] = options.Name
+	}
+
+	if options.IpAddressType != "" {
+		params["IpAddressType"] = options.IpAddressType
+	}
+
+	if options.Scheme != "" {
+		params["Scheme"] = options.Scheme
+	}
+
 	fmt.Println(params)
 
+	awsloadbalancer.PrepareSignatureV2query(params)
+
 	return
+}
+
+func prepareSubnets(params map[string]string, Subnets []string) {
+
+	for i := range Subnets {
+		n := strconv.Itoa(i + 1)
+		prefix := "Subnets.member." + n
+		params[prefix] = Subnets[i]
+	}
+
 }
 
 func (awsloadbalancer *Awsloadbalancer) Deleteloadbalancer(request interface{}) (resp interface{}, err error) {
@@ -34,7 +97,7 @@ func makeParamsWithVersion(action string) map[string]string {
 
 var timeNow = time.Now
 
-func (awsloadbalancer *Awsloadbalancer) PrepareSignatureV2query(params map[string]string, Region string, resp interface{}) error {
+func (awsloadbalancer *Awsloadbalancer) PrepareSignatureV2query(params map[string]string) error {
 
 	ElasticloadbalancingEndpoint := "https://elasticloadbalancing.amazonaws.com"
 
@@ -62,9 +125,9 @@ func (awsloadbalancer *Awsloadbalancer) PrepareSignatureV2query(params map[strin
 	}
 	defer r.Body.Close()
 
-	body, err := ioutil.ReadAll(r.Body)
+	resp, err := ioutil.ReadAll(r.Body)
 
-	fmt.Println(string(body))
+	fmt.Println(string(resp))
 
 	return xml.NewDecoder(r.Body).Decode(resp)
 }
