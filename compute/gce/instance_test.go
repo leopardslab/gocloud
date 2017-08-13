a34374a379bdf8d50aa6eb2de74cacc48f3eb3d1
PratikDhanave <i.pratikdhanave@gmail.com>
2017-08-14 00:24:02 +0530

    go test gce instance

--- compute/gce/instance_test.go
+++ compute/gce/instance_test.go
@@ -21,3 +21,26 @@ func TestStartnode(t *testing.T) {
 		fmt.Println(" Test fail")
 	}
 }
+
+
+
+func TestStopnode(t *testing.T) {
+	var gce GCE
+
+	stop := map[string]string{
+  "projectid":"sheltermap-1493101612061",
+	"instance":"instance-10",
+	"Zone": "us-west1-c",
+ }
+
+ resp, _ := gce.Stopnode(stop)
+
+ response := resp.(map[string]interface{})
+
+ if response["status"] == "200" {
+ 	fmt.Println(" Test pass")
+ } else {
+ 	fmt.Println(" Test fail")
+ }
+
+ }
