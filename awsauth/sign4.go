d96cb16b718dbf8ee494f71771de0b98eedfb929
Rumesh Eranga <rehrumesh@hotmail.com>
2017-10-29 07:51:16 +0530

    Code formatting for sign4.go

--- awsauth/sign4.go
+++ awsauth/sign4.go
@@ -11,31 +11,20 @@ import (
 )
 
 func SignatureV4(request *http.Request, request_parameters []byte, amztarget string, method string, region string, service string, host string, ContentType string, signedheaders string) (signrequest *http.Request) {
-
 	AccessKeyID := auth.Config.AWSAccessKeyID
-
 	SecretAccessKey := auth.Config.AWSSecretKey
 
 	t := time.Now().UTC()
-
 	XAmzDate := t.Format("20060102T150405Z")
-
 	date_stamp := t.Format("20060102")
-
 	canonical_uri := "/"
-
 	canonical_querystring := ""
-
 	canonical_headers := "content-type:" + ContentType + "\n" + "host:" + host + "\n" + "x-amz-date:" + XAmzDate + "\n" + "x-amz-target:" + amztarget + "\n"
-
 	payload_hashstring := sha256Hasher(request_parameters)
-
 	canonical_request := method + "\n" + canonical_uri + "\n" + canonical_querystring + "\n" + canonical_headers + "\n" + signedheaders + "\n" + payload_hashstring
 
 	algorithm := "AWS4-HMAC-SHA256"
-
 	credential_scope := date_stamp + "/" + region + "/" + service + "/" + "aws4_request"
-
 	string_to_sign := algorithm + "\n" + XAmzDate + "\n" + credential_scope + "\n" + sha256Hasher([]byte(canonical_request))
 
 	kDate := hmacSHA256([]byte("AWS4"+SecretAccessKey), date_stamp)
@@ -44,15 +33,11 @@ func SignatureV4(request *http.Request, request_parameters []byte, amztarget str
 	kSigning := hmacSHA256(kService, "aws4_request")
 
 	signature := hmacsignatureV4(kSigning, string_to_sign)
-
 	authorization_header := algorithm + " " + "Credential=" + AccessKeyID + "/" + credential_scope + ", " + "SignedHeaders=" + signedheaders + ", " + "Signature=" + signature
 
 	request.Header.Set("Content-Type", ContentType)
-
 	request.Header.Add("X-Amz-Date", XAmzDate)
-
 	request.Header.Add("X-Amz-Target", amztarget)
-
 	request.Header.Add("Authorization", authorization_header)
 
 	return request
