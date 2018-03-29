86aa4516db3d67306b17eee21842ff0c09ca1ea4
oddcn <oddzheng@126.com>
2018-03-29 13:57:58 +0800

    Ali compute ECS test case: Added less commonly used parameters for create node

--- compute/ecs/instance_test.go
+++ compute/ecs/instance_test.go
@@ -12,10 +12,12 @@ func init() {
 func TestCreatenode(t *testing.T) {
 	var aliEcs ECS
 	create := map[string]interface{}{
-		"RegionId":        "cn-qingdao",
-		"ImageId":         "centos_7_04_64_20G_alibase_201701015.vhd",
-		"InstanceType":    "ecs.xn4.small",
-		"SecurityGroupId": "sg-m5egbo9s5xb21kpu6nk2",
+		"RegionId":            "cn-qingdao",
+		"ImageId":             "centos_7_04_64_20G_alibase_201701015.vhd",
+		"InstanceType":        "ecs.xn4.small",
+		"SecurityGroupId":     "sg-m5egbo9s5xb21kpu6nk2",
+		"DataDisk.1.Size":     "20",
+		"DataDisk.1.DiskName": "testName",
 	}
 	_, err := aliEcs.Createnode(create)
 	if err != nil {
@@ -38,7 +40,7 @@ func TestStartnode(t *testing.T) {
 	t.Logf("Ali node is started successfully.")
 }
 
-func TestStopnode(t *testing.T)  {
+func TestStopnode(t *testing.T) {
 	var aliEcs ECS
 	stop := map[string]interface{}{
 		"InstanceId": "i-m5e3ee3z8wdy8ktdq591",
@@ -52,7 +54,7 @@ func TestStopnode(t *testing.T)  {
 	t.Logf("Ali node is stoped successfully.")
 }
 
-func TestRebootnode(t *testing.T)  {
+func TestRebootnode(t *testing.T) {
 	var aliEcs ECS
 	reboot := map[string]interface{}{
 		"InstanceId": "i-m5e3ee3z8wdy8ktdq591",
@@ -66,7 +68,7 @@ func TestRebootnode(t *testing.T)  {
 	t.Logf("Ali node is rebooted successfully.")
 }
 
-func TestDeletenode(t *testing.T)  {
+func TestDeletenode(t *testing.T) {
 	var aliEcs ECS
 	delete := map[string]interface{}{
 		"InstanceId": "i-m5e3ee3z8wdy8ktdq591",
