588e3b520c441337ede5796134e303928e1444d0
PratikDhanave <i.pratikdhanave@gmail.com>
2018-04-30 16:41:36 +0530

    initialised template for google cloud functions

---
+++ serverless/googlecloudfunctions/models.go
@@ -0,0 +1,28 @@
+package googlecloudfunctions
+
+
+func (googlecloudfunctions *Googlecloudfunctions) Create(request interface{}) (resp interface{}, err error) {
+
+  return resp, err
+}
+
+func (googlecloudfunctions *Googlecloudfunctions) delete(request interface{}) (resp interface{}, err error) {
+
+  return resp, err
+}
+
+func (googlecloudfunctions *Googlecloudfunctions) get(request interface{}) (resp interface{}, err error) {
+
+  return resp, err
+}
+
+func (googlecloudfunctions *Googlecloudfunctions) list(request interface{}) (resp interface{}, err error) {
+
+  return resp, err
+}
+
+
+func (googlecloudfunctions *Googlecloudfunctions) call(request interface{}) (resp interface{}, err error) {
+
+  return resp, err
+}
