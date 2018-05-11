75ec14fab4fa2052f6e16291e773e23f42dc0da8
Junqiu Zheng <oddzheng@126.com>
2018-05-11 23:29:03 +0800

    implemented ali dns module

--- dns/alidns/models.go
+++ dns/alidns/models.go
@@ -3,3 +3,33 @@ package alidns
 //Alidns represents Alidns attribute and method associates with it.
 type Alidns struct {
 }
+
+type CreateDNS struct {
+	DomainName string
+	RR         string
+	Type       string
+	Value      string
+	TTL        int
+	Priority   int
+	Line       string
+}
+
+type DeleteDNS struct {
+	RecordId string
+}
+
+type ListDNS struct {
+	PageNumber int
+	PageSize   int
+	KeyWord    string
+	GroupId    string
+}
+
+type ListResourceDNSRecordSets struct {
+	DomainName   string
+	PageNumber   int
+	PageSize     int
+	RRKeyWord    string
+	TypeKeyWord  string
+	ValueKeyWord string
+}
