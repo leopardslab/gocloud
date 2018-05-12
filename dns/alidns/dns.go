a2af19c7f6eccf324631039bd3505e001b8a2f99
Junqiu Zheng <oddzheng@126.com>
2018-05-12 15:04:51 +0800

    try builder pattern : ali create node

--- dns/alidns/dns.go
+++ dns/alidns/dns.go
@@ -7,150 +7,20 @@ import (
 
 // ListResourcednsRecordSets list resource DNS record sets accept map[string]interface{}
 func (alidns *Alidns) ListResourcednsRecordSets(request interface{}) (resp interface{}, err error) {
-	var options ListResourceDNSRecordSets
-
-	param := make(map[string]interface{})
-
-	param = request.(map[string]interface{})
-
-	for key, value := range param {
-		switch key {
-		case "DomainName":
-			options.DomainName = value.(string)
-		case "PageNumber":
-			switch value.(type) {
-			case int:
-				options.PageNumber = value.(int)
-			case string:
-				options.PageNumber, _ = strconv.Atoi(value.(string))
-			}
-		case "PageSize":
-			switch value.(type) {
-			case int:
-				options.PageSize = value.(int)
-			case string:
-				options.PageSize, _ = strconv.Atoi(value.(string))
-			}
-		case "RRKeyWord":
-			options.RRKeyWord = value.(string)
-		case "TypeKeyWord":
-			options.TypeKeyWord = value.(string)
-		case "ValueKeyWord":
-			options.ValueKeyWord = value.(string)
-		}
-	}
-	// Put all of options into params
-	params := aliauth.PutStructToMap(&options)
-
-	response := make(map[string]interface{})
-	err = aliauth.SignAndDoRequest("DescribeDomainRecords", params, response)
-	resp = response
 	return resp, err
 }
 
 // Listdns list DNS record accept map[string]interface{}
 func (alidns *Alidns) Listdns(request interface{}) (resp interface{}, err error) {
-	var options ListDNS
-
-	param := make(map[string]interface{})
-
-	param = request.(map[string]interface{})
-
-	for key, value := range param {
-		switch key {
-		case "PageNumber":
-			switch value.(type) {
-			case int:
-				options.PageNumber = value.(int)
-			case string:
-				options.PageNumber, _ = strconv.Atoi(value.(string))
-			}
-		case "PageSize":
-			switch value.(type) {
-			case int:
-				options.PageSize = value.(int)
-			case string:
-				options.PageSize, _ = strconv.Atoi(value.(string))
-			}
-		case "KeyWord":
-			options.KeyWord = value.(string)
-		case "GroupId":
-			options.GroupId = value.(string)
-		}
-	}
-	// Put all of options into params
-	params := aliauth.PutStructToMap(&options)
-
-	response := make(map[string]interface{})
-	err = aliauth.SignAndDoRequest("DescribeDomains", params, response)
-	resp = response
 	return resp, err
 }
 
 // Deletedns delete DNS record accept map[string]interface{}
 func (alidns *Alidns) Deletedns(request interface{}) (resp interface{}, err error) {
-	var options DeleteDNS
-
-	param := make(map[string]interface{})
-
-	param = request.(map[string]interface{})
-
-	for key, value := range param {
-		switch key {
-		case "RecordId":
-			options.RecordId = value.(string)
-		}
-	}
-	// Put all of options into params
-	params := aliauth.PutStructToMap(&options)
-
-	response := make(map[string]interface{})
-	err = aliauth.SignAndDoRequest("DeleteDomainRecord", params, response)
-	resp = response
 	return resp, err
 }
 
 // Createdns add DNS record accept map[string]interface{}
 func (alidns *Alidns) Createdns(request interface{}) (resp interface{}, err error) {
-	var options CreateDNS
-
-	param := make(map[string]interface{})
-
-	param = request.(map[string]interface{})
-
-	for key, value := range param {
-		switch key {
-		case "DomainName":
-			options.DomainName = value.(string)
-		case "RR":
-			options.RR = value.(string)
-		case "Type":
-			options.Type = value.(string)
-	    case "Value":
-			options.Value = value.(string)
-		case "TTL":
-			switch value.(type) {
-			case int:
-				options.TTL = value.(int)
-			case string:
-				options.TTL, _ = strconv.Atoi(value.(string))
-			}
-		case "Priority":
-			switch value.(type) {
-			case int:
-				options.Priority = value.(int)
-			case string:
-				options.Priority, _ = strconv.Atoi(value.(string))
-			}
-		case "Line":
-			options.Line = value.(string)
-		}
-	}
-	// Put all of options into params
-	params := aliauth.PutStructToMap(&options)
-
-	response := make(map[string]interface{})
-	err = aliauth.SignAndDoRequest("AddDomainRecord", params, response)
-	resp = response
 	return resp, err
 }
