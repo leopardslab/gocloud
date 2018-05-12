a9b008efd4f3ac915fb4ad25ff5dea30342d97d2
PratikDhanave <i.pratikdhanave@gmail.com>
2018-05-12 18:58:22 +0530

    aws dynomodb listtables

--- database/openstacknosql/openstacknosql.go
+++ database/openstacknosql/openstacknosql.go
@@ -5,6 +5,19 @@ func (openstacknosql *Openstacknosql) Listtables(request interface{}) (resp inte
 	return resp, err
 }
 
+func (openstacknosql *Openstacknosql) Deletetables(request interface{}) (resp interface{}, err error) {
+	return resp, err
+}
+
+func (openstacknosql *Openstacknosql) Describetables(request interface{}) (resp interface{}, err error) {
+	return resp, err
+}
+
+func (openstacknosql *Openstacknosql) Createtables(request interface{}) (resp interface{}, err error) {
+	return resp, err
+}
+
+
 /*
 func (rackspaceserverless *Rackspaceserverless) Createfunction(request interface{}) (resp interface{}, err error) {
 
