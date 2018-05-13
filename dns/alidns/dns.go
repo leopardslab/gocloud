cb7b6f033a9476dfc202018ef37b10b20cbfb8d9
Junqiu Zheng <oddzheng@126.com>
2018-05-13 14:24:27 +0800

    delete unused import

--- dns/alidns/dns.go
+++ dns/alidns/dns.go
@@ -1,10 +1,5 @@
 package alidns
 
-import (
-	"github.com/cloudlibz/gocloud/aliauth"
-	"strconv"
-)
-
 // ListResourcednsRecordSets list resource DNS record sets accept map[string]interface{}
 func (alidns *Alidns) ListResourcednsRecordSets(request interface{}) (resp interface{}, err error) {
 	return resp, err
