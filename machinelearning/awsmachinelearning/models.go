383ed0ced33cfde4063ee85f3cc901ab74044c5e
PratikDhanave <i.pratikdhanave@gmail.com>
2018-05-23 11:00:26 +0530

    aws ml api

--- machinelearning/awsmachinelearning/models.go
+++ machinelearning/awsmachinelearning/models.go
@@ -4,3 +4,18 @@ package awsmachinelearning
 type Awsmachinelearning struct{
 
 }
+
+type CreateMLModel struct {
+	MLModelID   string `json:"MLModelId"`
+	MLModelName string `json:"MLModelName"`
+	MLModelType string `json:"MLModelType"`
+	parameters Parameters`json:"Parameters"`
+	Recipe               string `json:"Recipe"`
+	RecipeURI            string `json:"RecipeUri"`
+	TrainingDataSourceID string `json:"TrainingDataSourceId"`
+}
+
+
+type Parameters  struct {
+  String string `json:"string"`
+}
