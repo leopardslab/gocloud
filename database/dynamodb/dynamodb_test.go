bc54d20a98d0fdafc1ff7aef9ff23a650d456eec
PratikDhanave <i.pratikdhanave@gmail.com>
2018-05-25 23:25:09 +0530

    testing

---
+++ database/dynamodb/dynamodb_test.go
@@ -0,0 +1,39 @@
+import "testing"
+import awsAuth "github.com/cloudlibz/gocloud/auth"
+
+func init() {
+	awsAuth.LoadConfig()
+}
+
+    func TestListtables(t *testing.T) {
+
+      var dynamodb Dynamodb
+
+      listtables := map[string]interface{}{
+        "Region":    "us-east-2",
+        "TableName": "hello",
+      }
+
+    	_, err := bigtable.Listtables(listtables)
+
+    	if err != nil {
+    		t.Errorf("Test Fail")
+    	}
+    }
+
+
+    func TestDeletetables(t *testing.T) {
+
+      var dynamodb Dynamodb
+
+      deletetables := map[string]interface{}{
+      "Region": "us-east-2",
+      "TableName" : "hello",
+      }
+
+      _, err := dynamodb.Deletetables(deletetables)
+
+      if err != nil {
+        t.Errorf("Test Fail")
+      }
+    }
