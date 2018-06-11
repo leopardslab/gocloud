026a2e1fdd94082f3ae4d17252b2c70a467575d8
PratikDhanave <i.pratikdhanave@gmail.com>
2018-06-11 19:44:51 +0530

    bigquery listdatasets

--- analytics/bigquery/bigquery.go
+++ analytics/bigquery/bigquery.go
@@ -23,3 +23,46 @@ func (bigquery *Bigquery) UpdateDatasets(request interface{}) (resp interface{},
 
 	return resp, err
 }
+
+
+//ListDatasets  list Datasets.
+func (vultralianalytics *Vultralianalytics) ListDatasets(request interface{}) (resp interface{}, err error) {
+
+	options := request.(map[string]string)
+
+	url := "https://www.googleapis.com/bigquery/v2/projects/" + options["projectId"] + "/datasets"
+
+	client := googleauth.SignJWT()
+
+	listdatasetsrequest, err := http.NewRequest("GET", url, nil)
+
+	listdatasetsrequestparam := listdatasetsrequest.URL.Query()
+
+	if options["filter"] != "" {
+		listdatasetsrequestparam.Add("filter", options["filter"])
+	}
+
+	if options["maxResults"] != "" {
+		listdatasetsrequestparam.Add("maxResults", options["maxResults"])
+	}
+
+	if options["pageToken"] != "" {
+		listdatasetsrequestparam.Add("pageToken", options["pageToken"])
+	}
+
+	listdatasetsrequest.URL.RawQuery = listdatasetsrequestparam.Encode()
+
+	listdatasetsrequest.Header.Set("Content-Type", "application/json")
+
+	listdatasetsrequestresp, err := client.Do(listdatasetsrequest)
+
+	defer listdatasetsrequestresp.Body.Close()
+
+	body, err := ioutil.ReadAll(listdatasetsrequestresp.Body)
+
+	listdatasetsrequestrespresponse := make(map[string]interface{})
+	listdatasetsrequestrespresponse["status"] = listdatasetsrequestresp.StatusCode
+	listdatasetsrequestrespresponse["body"] = string(body)
+	resp = listdatasetsrequestrespresponse
+	return resp, err
+}
