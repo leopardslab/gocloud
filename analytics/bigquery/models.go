9516383b4639072c9175278526ef6771d5b1cb3e
PratikDhanave <i.pratikdhanave@gmail.com>
2018-06-13 20:47:06 +0530

    bigquery test

--- analytics/bigquery/models.go
+++ analytics/bigquery/models.go
@@ -34,6 +34,7 @@ type CreateDatasets struct {
 	id                       string           `json:"id"`
 	kind                     string           `json:"kind"`
 	lastModifiedTime         string           `json:"lastModifiedTime"`
+	creationTime						 string           `json:"creationTime"`
 	location                 string           `json:"location"`
 	selfLink                 string           `json:"selfLink"`
 }
