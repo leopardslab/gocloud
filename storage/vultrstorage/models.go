db0d2d569a6c0eb933e598e2a740e3fde798fc76
Junqiu Zheng <oddzheng@126.com>
2018-06-15 00:37:05 +0800

    vultr create attach detach  delete disk

--- storage/vultrstorage/models.go
+++ storage/vultrstorage/models.go
@@ -10,3 +10,22 @@ type CreateSnapshot struct {
 type DeleteSnapshot struct {
 	SNAPSHOTID string
 }
+
+type CreateDisk struct {
+	DCID    int    // DCID of the location to create this subscription in.  See /v1/regions/list
+	size_gb int    // Size (in GB) of this subscription.
+	label   string // (optional) Text label that will be associated with the subscription
+}
+
+type AttachDisk struct {
+	SUBID           int // ID of the block storage subscription to attach
+	attach_to_SUBID int // ID of the VPS subscription to mount the block storage subscription to
+}
+
+type DetachDisk struct {
+	SUBID int // ID of the block storage subscription to detach
+}
+
+type DeleteDisk struct {
+	SUBID int // ID of the block storage subscription to delete
+}
--- storage/vultrstorage/storage.go
+++ storage/vultrstorage/storage.go
@@ -7,11 +7,27 @@ import (
 
 // Createdisk function creates a new disk.
 func (vultrStorage *VultrStorage) Createdisk(request interface{}) (resp interface{}, err error) {
+	param := make(map[string]interface{})
+	param = request.(map[string]interface{})
+
+	response := make(map[string]interface{})
+
+	err = vultrauth.SignAndDoRequest(http.MethodPost, "/v1/block/create", param, response)
+
+	resp = response
 	return resp, err
 }
 
 // Deletedisk function deletes a disk.
 func (vultrStorage *VultrStorage) Deletedisk(request interface{}) (resp interface{}, err error) {
+	param := make(map[string]interface{})
+	param = request.(map[string]interface{})
+
+	response := make(map[string]interface{})
+
+	err = vultrauth.SignAndDoRequest(http.MethodPost, "/v1/block/delete", param, response)
+
+	resp = response
 	return resp, err
 }
 
@@ -43,10 +59,26 @@ func (vultrStorage *VultrStorage) Deletesnapshot(request interface{}) (resp inte
 
 // Attachdisk function function attaches a disk to a Vultr server.
 func (vultrStorage *VultrStorage) Attachdisk(request interface{}) (resp interface{}, err error) {
+	param := make(map[string]interface{})
+	param = request.(map[string]interface{})
+
+	response := make(map[string]interface{})
+
+	err = vultrauth.SignAndDoRequest(http.MethodPost, "/v1/block/attach", param, response)
+
+	resp = response
 	return resp, err
 }
 
 // Detachdisk function function detaches a disk from a Vultr server.
 func (vultrStorage *VultrStorage) Detachdisk(request interface{}) (resp interface{}, err error) {
+	param := make(map[string]interface{})
+	param = request.(map[string]interface{})
+
+	response := make(map[string]interface{})
+
+	err = vultrauth.SignAndDoRequest(http.MethodPost, "/v1/block/detach", param, response)
+
+	resp = response
 	return resp, err
 }
