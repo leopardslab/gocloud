e0b8ccdbc44595aa457d1928471356a7e1b9b29b
Junqiu Zheng <oddzheng@126.com>
2018-06-22 00:48:46 +0800

    vultr list dns

--- dns/vultrdns/dns.go
+++ dns/vultrdns/dns.go
@@ -3,15 +3,34 @@ package vultrdns
 import (
 	"github.com/cloudlibz/gocloud/vultrauth"
 	"net/http"
+	"fmt"
 )
 
 // ListResourcednsRecordSets function lists DNS record sets.
 func (vultrDNS *VultrDNS) ListResourcednsRecordSets(request interface{}) (resp interface{}, err error) {
+	fmt.Println("\nThis API is not provided by Vultr cloud")
 	return resp, err
 }
 
 // Listdns function lists DNS records.
 func (vultrDNS *VultrDNS) Listdns(request interface{}) (resp interface{}, err error) {
+	param := make(map[string]interface{})
+	param = request.(map[string]interface{})
+
+	var domain string
+
+	for key, value := range param {
+		switch key {
+		case "domain":
+			domain = value.(string)
+		}
+	}
+
+	response := make(map[string]interface{})
+
+	err = vultrauth.SignAndDoRequest(http.MethodGet, "/v1/dns/records?domain="+domain, param, response)
+
+	resp = response
 	return resp, err
 }
 
