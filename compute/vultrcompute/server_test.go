89495ef1b60c8283951a4fed7c71634eaf2cd0ca
Junqiu Zheng <oddzheng@126.com>
2018-07-19 23:29:41 +0800

    vultr resp create node

--- compute/vultrcompute/server_test.go
+++ compute/vultrcompute/server_test.go
@@ -97,3 +97,28 @@ func TestVultrCompute_ListNode(t *testing.T) {
 	}
 	t.Logf("Vultr node list: %s", response["body"])
 }
+
+func TestParseCreateDiskResp(t *testing.T) {
+	var vultrServer VultrCompute
+	create := map[string]interface{}{
+		"DCID":      1,
+		"VPSPLANID": 201,
+		"OSID":      127,
+	}
+	resp, err := vultrServer.CreateNode(create)
+	if err != nil {
+		t.Errorf("CreateNode Test Fail: %s", err)
+		return
+	}
+	response := resp.(map[string]interface{})
+	if response["status"] != 200 {
+		t.Errorf("status code: %d\n response body: %s\n", response["status"], response["body"])
+		return
+	}
+	createNodeResp, err := ParseCreateNodeResp(response["body"])
+	if err != nil {
+		t.Errorf("CreateNode Test Fail: %s", err)
+		return
+	}
+	t.Logf("Vultr node is created successfully.\n %+v", createNodeResp)
+}
