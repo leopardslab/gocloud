c24eeb04f9404b642dc456a849a9e6a9e9657332
anuragparida <anuragparida37@gmail.com>
2018-10-25 03:57:33 +0530

    remove redundant comments

--- main.go
+++ main.go
@@ -9,34 +9,6 @@ func main() {
 
 	digioceancloud := gocloud.DigitalOceanProvider()
 
-	/*
-	   	image := map[string]interface{}{
-	       "Slug": "ubuntu-16-04-x64",
-	     }
-
-	     create := map[string]interface{}{
-	       "Name":   "example.com",
-	       "Region": "nyc3",
-	       "Size":   "s-1vcpu-1gb",
-	       "Image": image,
-	       "SSHKeys": nil,
-	       "Backups": false,
-	       "IPv6": true,
-	       "UserData": nil,
-	       "PrivateNetworking": nil,
-	       "Volumes": nil,
-	       "Monitoring": false,
-	       "Tags": []string{"web"},
-	     }
-
-	    resp, err := digioceancloud.CreateNode(create)
-	    if err != nil{
-	   	 fmt.Println(err)
-	    }
-	    response := resp.(map[string]interface{})
-	    fmt.Println(response["body"])
-	*/
-
 	stop := map[string]string{
 		"ID": "102694881",
 	}
