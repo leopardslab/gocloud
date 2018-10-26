7e8749765bdb7b9e50e7bee01dba9476aad6b712
Siddh Raman Pant <25429745+siddhpant@users.noreply.github.com>
2018-10-26 14:31:20 +0530

    Update aliserverless.go

--- serverless/vultrserverless/aliserverless.go
+++ serverless/vultrserverless/aliserverless.go
@@ -2,30 +2,25 @@ package vultrserverless
 
 //GetFunction  describe serverless function.
 func (vultrserverless *Vultrserverless) GetFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //CreateFunction  create serverless function.
 func (vultrserverless *Vultrserverless) CreateFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //CallFunction  call serverless function.
 func (vultrserverless *Vultrserverless) CallFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //ListFunction  list serverless function.
 func (vultrserverless *Vultrserverless) ListFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //DeleteFunction  delete serverless function.
 func (vultrserverless *Vultrserverless) DeleteFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
