a57cfa1bc6fc063504c471bc209918acb9103866
Siddh Raman Pant <25429745+siddhpant@users.noreply.github.com>
2018-10-26 14:30:50 +0530

    Update digioceanserverless.go

--- serverless/digioceanserverless/digioceanserverless.go
+++ serverless/digioceanserverless/digioceanserverless.go
@@ -2,30 +2,25 @@ package digioceanserverless
 
 //GetFunction  describe serverless function.
 func (digioceanserverless *Digioceanserverless) GetFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //CreateFunction  create serverless function.
 func (digioceanserverless *Digioceanserverless) CreateFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //CallFunction  call serverless function.
 func (digioceanserverless *Digioceanserverless) CallFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //ListFunction  list serverless function.
 func (digioceanserverless *Digioceanserverless) ListFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //DeleteFunction  delete serverless function.
 func (digioceanserverless *Digioceanserverless) DeleteFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
