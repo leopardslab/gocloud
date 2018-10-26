bd1d37f2b327b46e44f248515c67e74f8e832340
Siddh Raman Pant <25429745+siddhpant@users.noreply.github.com>
2018-10-26 14:30:00 +0530

    Update aliserverless.go

--- serverless/aliserverless/aliserverless.go
+++ serverless/aliserverless/aliserverless.go
@@ -2,30 +2,25 @@ package aliserverless
 
 //GetFunction  describe serverless function.
 func (aliserverless *Aliserverless) GetFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //CreateFunction  create serverless function.
 func (aliserverless *Aliserverless) CreateFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //CallFunction  call serverless function.
 func (aliserverless *Aliserverless) CallFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //ListFunction  list serverless function.
 func (aliserverless *Aliserverless) ListFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //DeleteFunction  delete serverless function.
 func (aliserverless *Aliserverless) DeleteFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
