a62a9bc77d233586f4b5a4ff4f646571b15a8797
Siddh Raman Pant <25429745+siddhpant@users.noreply.github.com>
2018-10-26 14:34:24 +0530

    Update azurealianalytics.go

--- analytics/azurealianalytics/azurealianalytics.go
+++ analytics/azurealianalytics/azurealianalytics.go
@@ -2,24 +2,20 @@ package azurealianalytics
 
 //CreateDatasets Create Datasets.
 func (azurealianalytics *Azurealianalytics) CreateDatasets(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //DeleteDatasets delete Datasets.
 func (azurealianalytics *Azurealianalytics) DeleteDatasets(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //GetDatasets get Datasets.
 func (azurealianalytics *Azurealianalytics) GetDatasets(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //UpdateDatasets  Update Datasets.
 func (azurealianalytics *Azurealianalytics) UpdateDatasets(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
