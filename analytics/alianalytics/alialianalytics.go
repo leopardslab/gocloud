ed31224c630e084b0d1805cd0df4f90c2472821e
tintinmovie <31559358+tintinmovie@users.noreply.github.com>
2018-10-26 20:07:01 +1300

    Update alialianalytics.go

--- analytics/alianalytics/alialianalytics.go
+++ analytics/alianalytics/alialianalytics.go
@@ -2,24 +2,17 @@ package alialianalytics
 
 //CreateDatasets Create Datasets.
 func (alialianalytics *Alialianalytics) CreateDatasets(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
-
 //DeleteDatasets delete Datasets.
 func (alialianalytics *Alialianalytics) DeleteDatasets(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
-
 //GetDatasets get Datasets.
 func (alialianalytics *Alialianalytics) GetDatasets(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
-
 //UpdateDatasets  Update Datasets.
 func (alialianalytics *Alialianalytics) UpdateDatasets(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
