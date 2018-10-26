885f689567c9f8bda0b5eae42e85bb4ac9ce22e8
Siddh Raman Pant <25429745+siddhpant@users.noreply.github.com>
2018-10-26 14:32:08 +0530

    Update rackspaceserverless.go

--- serverless/rackspaceserverless/rackspaceserverless.go
+++ serverless/rackspaceserverless/rackspaceserverless.go
@@ -2,30 +2,25 @@ package rackspaceserverless
 
 //GetFunction  describe serverless function.
 func (rackspaceserverless *Rackspaceserverless) GetFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //CreateFunction  create serverless function.
 func (rackspaceserverless *Rackspaceserverless) CreateFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //CallFunction  call serverless function.
 func (rackspaceserverless *Rackspaceserverless) CallFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //ListFunction  list serverless function.
 func (rackspaceserverless *Rackspaceserverless) ListFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //DeleteFunction  delete serverless function.
 func (rackspaceserverless *Rackspaceserverless) DeleteFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
