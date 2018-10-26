85d9e204266083d9c7e9a290dedcf6c0c8eb1fd4
Siddh Raman Pant <25429745+siddhpant@users.noreply.github.com>
2018-10-26 15:42:33 +0530

    Typo breaks whole build 😕

--- streamdataprocessing/clouddataflow/clouddataflow_test.go
+++ streamdataprocessing/clouddataflow/clouddataflow_test.go
@@ -20,7 +20,7 @@ func TestListStream(t *testing.T) {
 func TestDescribeStream(t *testing.T) {
 	var clouddataflow Clouddataflow
 
-	describedtream := map[string]string{
+	describestream := map[string]string{
 		"Project": "gocloud-206919",
 		"JobId":   "2018-07-27_08_37_46-11774589915372519551",
 	}
