aab8f130b32246ecd686eef32e035fe711552a58
Siddh Raman Pant <25429745+siddhpant@users.noreply.github.com>
2018-10-26 14:31:46 +0530

    Update openstackserverless.go

--- serverless/openstackserverless/openstackserverless.go
+++ serverless/openstackserverless/openstackserverless.go
@@ -2,30 +2,25 @@ package openstackserverless
 
 //GetFunction  describe serverless function.
 func (openstackserverless *Openstackserverless) GetFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //CreateFunction  create serverless function.
 func (openstackserverless *Openstackserverless) CreateFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //CallFunction  call serverless function.
 func (openstackserverless *Openstackserverless) CallFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //ListFunction  list serverless function.
 func (openstackserverless *Openstackserverless) ListFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //DeleteFunction  delete serverless function.
 func (openstackserverless *Openstackserverless) DeleteFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
