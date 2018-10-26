bab72ffe98e2c8cd6afd93024ecb9912e36cb9dc
Siddh Raman Pant <25429745+siddhpant@users.noreply.github.com>
2018-10-26 14:30:29 +0530

    Update azureserverless.go

--- serverless/azureserverless/azureserverless.go
+++ serverless/azureserverless/azureserverless.go
@@ -2,30 +2,25 @@ package azureserverless
 
 //GetFunction  describe serverless function.
 func (azureserverless *Azureserverless) GetFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //CreateFunction  create serverless function.
 func (azureserverless *Azureserverless) CreateFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //CallFunction  call serverless function.
 func (azureserverless *Azureserverless) CallFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //ListFunction  list serverless function.
 func (azureserverless *Azureserverless) ListFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
 
 //DeleteFunction  delete serverless function.
 func (azureserverless *Azureserverless) DeleteFunction(request interface{}) (resp interface{}, err error) {
-
 	return resp, err
 }
